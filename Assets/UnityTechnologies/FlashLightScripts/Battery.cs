20e2329d6bb99be7356ce47ff6ef488db2f54877
TrojanBlazing <145752343+TrojanBlazing@users.noreply.github.com>
2024-01-27 00:31:55 +0530

    FLashLight
    FlashLight

---
+++ Assets/UnityTechnologies/FlashLightScripts/Battery.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField]
+    int BatteryWeight;
+    [SerializeField]
+    GameObject [] HoverObject;
+
+    [SerializeField]
+    KeyCode CK = KeyCode.E;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void OnMouseOver()
+    {
+        foreach (GameObject obj in HoverObject)
+        {
+            obj.SetActive(true);
+        }
+        if (Input.GetKeyDown(CK))
+        {
+            FindObjectOfType<FlashLight>().GainBattery(BatteryWeight);
+            Destroy(this.gameObject);
+        }
+    }
+    public void OnMouseExit()
+    {
+        foreach(GameObject obj in HoverObject)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
