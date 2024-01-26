58a8363c6800678b01ecc9e726907ac3a1dde87b
sushant <151763577+ParadiseSan@users.noreply.github.com>
2024-01-27 02:10:20 +0530

    Commit0.1

---
+++ Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,22 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera virtualCamera; // Drag your virtual camera here in the Unity Editor
+
+    [SerializeField] Transform hidingPlayer; // Drag your new follow target here in the Unity Editor
+    [SerializeField] Transform originalPlayer;
+
+    public void SwitchPlayerCamWhenHiding()
+    {
+        virtualCamera.Follow = hidingPlayer;
+    }
+
+    public void SwitchPlayerCamToOriginal()
+    {
+        virtualCamera.Follow = originalPlayer;
+    }
+}
